5c5d2f947a3e297d81e2b88650d496d60765679d
stephen-smurphy <85882285+stephen-smurphy@users.noreply.github.com>
2021-12-30 18:26:19 +0000

    Added button with event handling

---
+++ testing.js
@@ -0,0 +1,30 @@
+function addContent() {
+    const container = document.querySelector("#container");
+
+    const paragraph = document.createElement("P");
+    paragraph.textContent = "I am red!";
+    paragraph.style.color = "red";
+
+    const title = document.createElement("h3");
+    title.textContent = "I'm a blue h3!";
+    title.style.color = "blue";
+
+    container.appendChild(paragraph);
+    container.appendChild(title);
+
+    const innerContainer = document.createElement("div")
+    innerContainer.style.border = "1px solid black";
+
+    const title2 = document.createElement("h1");
+    title2.textContent = "I'm in a div";
+    innerContainer.appendChild(title2);
+
+    const p2 = document.createElement("p");
+    p2.textContent = "ME TOO!";
+    innerContainer.appendChild(p2);
+
+    container.appendChild(innerContainer);
+}
+
+var button = document.getElementById("button1");
+button.onclick = addContent;
